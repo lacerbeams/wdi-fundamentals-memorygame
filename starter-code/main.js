bb27725fb811338e59b09901ecfcf57b8631518e
Lacey Madison <laceyannmadison@gmail.com>
2016-11-19 13:35:56 -0800

    added initial game logic

--- starter-code/main.js
+++ starter-code/main.js
@@ -3,3 +3,8 @@ var card0ne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
+
+if (cardTwo === cardFour);
+window.alert('You found a match!');
+if (cardTwo !== cardFour);
+window.alert('Sorry, try again.');
