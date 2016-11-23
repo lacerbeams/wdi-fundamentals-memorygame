d663af1a8f3ab40a9194469f271adb9331e06e37
Lacey Madison <laceyannmadison@gmail.com>
2016-11-22 22:14:35 -0800

    added js to create cards

--- starter-code/main.js
+++ starter-code/main.js
@@ -3,8 +3,18 @@ var card0ne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
-
+/*
 if (cardTwo === cardFour);
 window.alert('You found a match!');
 if (cardTwo !== cardFour);
 window.alert('Sorry, try again.');
+*/
+
+var gameboard = document.getElementById('game-board');
+function createCards(gameboard){
+for (var i = 0; i < 4; i++){
+var newDiv = document.createElement('div');
+newDiv.className = "card";
+gameboard.appendChild(newDiv);
+}}
+createCards(gameboard);
